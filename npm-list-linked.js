378860c596918bfd9b74e61bf7060c3df2673182
Kristoffer Lundén <kristoffer.lunden@gmail.com>
2019-01-15 12:31:49 +0100

    0.0.4 Slightly better output

--- npm-list-linked.js
+++ npm-list-linked.js
@@ -13,20 +13,26 @@ function getLinked(cwd) {
         });
 }
 
-function printLinked(cwd, level = 0) {
+function printLinked(cwd, level = 1) {
     const modules = path.join(cwd, 'node_modules');
     const indentation = ' '.repeat(level * 4);
     const linked = getLinked(modules);
-    linked
-        .forEach(link => {
-            const version = fs.readJsonSync(path.join(modules, link, 'package.json')).version;
-            console.log(indentation + link, version);
-            printLinked(path.join(modules, link), level + 1);
-        });
+
+    if(level === 1) {
+        if(linked.length > 0) {
+            console.log(`Linked packages in ${cwd}:`);
+        } else {
+            console.log('No linked packages in', cwd);
+        }
+    }
+
+    linked.forEach(link => {
+        const version = fs.readJsonSync(path.join(modules, link, 'package.json')).version;
+        console.log(indentation + link, version);
+        printLinked(path.join(modules, link), level + 1);
+    });
 }
 
 const cwd = process.argv.length > 2 ? process.argv[2] : process.cwd();
 
-console.log('Linked packages in', cwd);
-
-printLinked(cwd, 1);
+printLinked(cwd);
