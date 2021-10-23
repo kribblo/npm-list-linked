9fa13457d74e84786398e129ff718aff83138a7c
Emmanuel Mahuni <emahuni@users.noreply.github.com>
2021-10-23 12:21:08 +0200

    fix: error infinite recursive globbing
    It fixes this issue:
    
    Linked packages in /home/em/projects/app:
        @emanimation/common 1.1.0
            @emanimation/common 1.1.0
                @emanimation/common 1.1.0
                    @emanimation/common 1.1.0
                        @emanimation/common 1.1.0
                            @emanimation/common 1.1.0
                                @emanimation/common 1.1.0
                                    @emanimation/common 1.1.0
                                         .... // does that forever, or maybe until machine crashes!

--- npm-list-linked.js
+++ npm-list-linked.js
@@ -8,7 +8,7 @@ const NODE_MODULES = 'node_modules';
 
 function printLinked(cwd, level = 1) {
     const indentation = ' '.repeat(level * 4);
-    const linked = getLinked();
+    const linked = getLinked(cwd);
 
     if(level === 1) {
         if(linked.length > 0) {
